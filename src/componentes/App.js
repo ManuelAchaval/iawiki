c782573e9ef40c47fe732db604f7697219219160
ManuelAchaval <manuachaval@hotmail.com>
2023-02-12 15:55:16 -0300

    agrego como se vera la pag

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,13 +1,16 @@
 import Nav from "./nav/Nav"
-import Footer from "./footer/Footer"
+import "./nav/Footer.css"
+import "./nav/Nav.css"
+
 const App = () => {
     return (
         <div>
-           <Nav/>
-            <p>
+            <Nav type="nav"/>
+            <p style={{textAlign:"center"}}>
                 Acá se pueden consultar sobre temas varios y también buscar sobre un tema especifico utilizando nuestro sistema de etiquetas
             </p>
-            <Footer/>
+            <Nav/>
+            <Nav type="footer"/>
         </div>
     )
 }
--- src/componentes/nav/Nav.jsx
+++ src/componentes/nav/Nav.jsx
@@ -1,19 +1,53 @@
-const Nav = () => {
-    return (
-        <div>
-            <nav>
+
+const Nav = (param) => {
+
+    if (param.type === "nav") {
+        return (
+            <div>
+                <nav>
 
 
-                <h1>
-                    IAwiki
-                </h1>
+                    <h1>
+                        IAwiki
+                    </h1>
 
-                <ul>
-                    <li>Inicio</li>
-                    <li>Busqueda</li>
-                </ul>
-            </nav>
-        </div>
-    )
+                    <ul>
+                        <li><a href="">Inicio</a> </li>
+                        <li><a href="">Busqueda</a> </li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
+    else if (param.type === "footer") {
+        return (
+            <div className='footer'>
+                <h2>Contactanos!</h2>
+                <h2>Sumate a nuestro equipo</h2>
+            </div>
+        )
+    } else {
+        return (
+
+            <div className="contenedor">
+                <div className="navCard">
+                    <div className="porTitulo">
+                        <h2>Buscar por titulo</h2>
+                        <input type="search" name="" id="porTitulo" />
+                    </div>
+                    <div className="porTag">
+                        <h2>Buscar por Tag</h2>
+                        <input type="search" name="" id="porTag" />
+                    </div>
+                </div>
+                <div className="contieneTag">
+                    <div className="card"><h3>titulo </h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit, alias.</div>
+                    <div className="card"><h3>titulo </h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit officiis alias deserunt fugiat quidem eveniet.</div>
+                    <div className="card"><h3>titulo </h3>Lorem ipsum dolor sit amet.</div>
+                    <div className="card"><h3>titulo </h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam, et nesciunt? Necessitatibus, hic neque officia blanditiis sapiente, deleni</div>
+                </div>
+            </div>
+        )
+    }
 }
 export default Nav
